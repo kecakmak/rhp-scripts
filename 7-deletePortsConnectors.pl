245603a3c7470e4ef73baf23f1d765f7ad53fa64
kecakmak <85883537+kecakmak@users.noreply.github.com>
2023-09-25 09:29:44 +0300

    Add files via upload

--- 7-deletePortsConnectors.pl
+++ 7-deletePortsConnectors.pl
@@ -63,7 +63,7 @@ if (($partToDelete ne "") && ($partToDelete ne "NULL")){
 
 	my $portCheck = "false";
 
-	if (($portToDelete ne "") || ($portToDelete ne "NULL")){
+	if (($portToDelete ne "") && ($portToDelete ne "NULL")){
 		my $fromPortID_all = ""; 
 		my $fromPortRMID = "";
 		my $fromPortID = "";
@@ -82,7 +82,7 @@ if (($partToDelete ne "") && ($partToDelete ne "NULL")){
 		if ($portCheck eq "true"){
 			if (($connectorRequested eq "") || ($connectorRequested eq "NULL")){
 				print "all connectors of this port and also the port itself: $portToDelete will be deleted\n"; 
-				deletePort($fullPath, $portToDelete, $fromPortID, $parentFromBlockID);
+				deletePort($fullPath, $portToDelete, $fromPortID, $parentFromBlockID, $parentFromBlockPart, $parentFromBlock);
 			}
 			else {
 				my $connectorOfPort = getConnectorOfPort($fullPath, $fromPortName, $fromPortID, $parentFromBlockPart, $parentFromBlock, $otherPart);
@@ -110,6 +110,16 @@ if (($partToDelete ne "") && ($partToDelete ne "NULL")){
 			print "Wrong Port Name Provided\n";
 		}
 	
+	}
+	else {
+		print "Part: $partToDelete will be deleted\n";
+		for (@portNames_arr) {
+			my $portNameAndID = $_;
+			my ($fromPortName, $fromPortID_all) = split(/\|\|/, $portNameAndID);		
+			my ($fromPortID, $fromPortRMID) = split("RM_SEPERATOR", $fromPortID_all);
+			deletePort($fullPath, $fromPortName, $fromPortID, $parentFromBlockID, $parentFromBlockPart, $parentFromBlock);
+		}
+		deletePart($fullPath, $partToDelete, $parentFromBlockPart, $parentFromBlockID);
 	}
 
 }
@@ -229,6 +239,158 @@ sub justListPorts{
 
 
 
+sub deletePart {
+	my $fullPath = $_[0];
+	my $partName = $_[1]; 
+	my $partID = $_[2];
+	my $parentBlockPartID = $_[3];
+	
+	print "PART DELETE: \n $fullPath\n $partName \n $partID \n $parentBlockPartID \n";
+
+
+
+	
+	print "\n\n\n$fullPath\n$partName\n$partID\n$parentBlockPartID\n";
+	
+	my $blockFiles = qx/ find $fullPath \-type f  \| xargs -n1 awk \'\/<_id type=\"a\">$partID<\\\/_id>\/\,\/<\\\/IPart>\/ \{printf FILENAME \"  \"\; print\}\'  / ;
+	$blockFiles=~s/\t/:/ig;
+	
+	my ($blockFile,$junk) = split(":::",$blockFiles);
+	($blockFile,$junk) = split("::",$blockFiles); #sometimes there are 3 spaces, sometimes two... 
+	$blockFile=~s/\s+$//; #right trim to get rid of any spaces at the end 
+	
+	print "Filename: $blockFile\n";
+
+	if ($blockFile eq "") {
+		print "Error with the connection information. Please make sure to use correct connection name for the given part and port\n";
+		exit (-1);
+	}
+	
+	my $fileContents = getFileContents($blockFile);
+
+	my $rmID = findRmid($partName, $fileContents, "IPart"); 
+	
+
+	my @fileContents_arr = split("\n",$fileContents);
+	
+	my $inPart = "false"; 
+	my $inCorrectPart = "false";
+	my $inElement = "false";
+	my $inCorrectElement = "false";
+	
+	open(BUFFERINIT, ">",$blockFile);
+	print BUFFERINIT "";
+	close(BUFFERINIT);
+	
+	open(BUFFER, ">>", $blockFile); 
+	
+	foreach (@fileContents_arr) {
+		chomp($_);
+		my $line = $_; 
+		if (index($line,"<IPart type=\"e\">")!=-1) {$inPart = "true";}
+		if (index($line,"<ELEMENT>")!=-1){$inElement = "true";}
+		if (index($line,"<\/IPart>")!=-1){
+			$inPart = "false";
+			$inCorrectPart = "false";
+		}
+		if (index($line,"<\/ELEMENT")!=-1){
+			$inElement = "false";
+			$inCorrectElement = "false";
+		}
+		
+		if (($inPart eq "false") && ($inElement eq "false")) {
+			print BUFFER $line . "\n"; 			
+		}
+		
+		else {
+# in Part lets check if that is the right one. 
+			if (index($line,"<_id type=\"a\">$partID")!=-1){
+				$inCorrectPart = "true"; 
+			}
+			
+			if (index($line, "<ID>$rmID</ID>")!=-1){
+				$inCorrectElement = "true";
+			}
+
+			if (($inCorrectPart eq "true") || ($inCorrectElement eq "true")) {
+				next;
+			}
+			else {
+				print BUFFER $line . "\n";
+			}
+		}
+	
+	}
+	close (BUFFER); 	
+
+
+	my $fileContents = getFileContents($blockFile);
+	my @fileContents_arr = split("\n",$fileContents);
+	
+	open (BUFFERR, ">", $blockFile);
+	my $prev_line = "";
+	my $prev_line_print = "";
+	my $rm_prev_line = "";
+	my $rm_prev_line_print = "";
+	my $secSkip = "false";
+	foreach(@fileContents_arr) {
+	
+		my $current_line = $_; 	
+		chomp($current_line);
+		$current_line =~s/\s//ig; 
+		$current_line =~s/\n//ig;
+		
+		if (index($current_line,"<IParttype=\"e\">") !=-1) {
+			$prev_line = $current_line;
+			$prev_line_print = $_;
+			next;
+		}
+		if ($prev_line ne "") {
+			if (index($prev_line . $current_line,"<IParttype=\"e\"><\/IPart>") !=-1){
+				$prev_line = "";
+				$prev_line_print = ""; 
+				next;
+			}
+			else {
+				print BUFFERR "$prev_line_print\n";
+				$prev_line = "";
+				$prev_line_print = ""; 
+				}
+		}
+		
+		
+		if (index($current_line,"<ELEMENT>") !=-1) {
+			$rm_prev_line = $current_line;
+			$rm_prev_line_print = $_;
+			next;
+		}
+		if ($rm_prev_line ne "") {
+			if ((index($rm_prev_line,"<ELEMENT>")!=-1) && (index($current_line,"<\/ELEMENT>")!=-1)){
+				$rm_prev_line = "";
+				$rm_prev_line_print = ""; 
+				next;
+			}
+			else {
+				print BUFFERR "$rm_prev_line_print\n";
+				$rm_prev_line = "";
+				$rm_prev_line_print = ""; 
+				}
+		}
+		
+	
+
+		print BUFFERR "$_\n";
+
+	}	
+	close (BUFFERR);
+	
+	
+	fixRhapsodyIndicies($blockFile);	
+	
+
+	
+
+}
 
 
 
@@ -237,11 +399,165 @@ sub justListPorts{
 sub deletePort {
 	my $fullPath = $_[0];
 	my $portName = $_[1]; 
-	my $allID = $_[2];
+	my $portID = $_[2];
 	my $parentBlockPartID = $_[3];
-#	my $parentBlockName = $_[4];
+	my $fromPortName = $portName;
+	my $fromPortID = $portID;
+	my $parentFromBlockPart = $_[4];
+	my $parentFromBlock = $_[5];
+
+
+# first get the connectors attached to the port 
+	my $connectorOfPort = getConnectorOfPort($fullPath, $fromPortName, $fromPortID, $parentFromBlockPart, $parentFromBlock, $otherPart);
+	my @connectors_arr = split(/\|\|/, $connectorOfPort); 
+
+# then delete each connector 
+	foreach (@connectors_arr){
+		my ($connectorName, $connectorID) = split("ID_SEPERATOR", $_); 
+		$connectorID=~s/GUID//ig;
+		deleteConnector($fullPath,$connectorName,$connectorID);
+	}
+# Then delete the port. 
 	
-	print "\n\n\n$fullPath\n$portName\n$allID\n$parentBlockPartID\n";
+	print "\n\n\n$fullPath\n$portName\n$portID\n$parentBlockPartID\n";
+	
+	my $blockFiles = qx/ find $fullPath \-type f  \| xargs -n1 awk \'\/<_id type=\"a\">$portID<\\\/_id>\/\,\/<\\\/IPort>\/ \{printf FILENAME \"  \"\; print\}\'  / ;
+	$blockFiles=~s/\t/:/ig;
+	
+	my ($blockFile,$junk) = split(":::",$blockFiles);
+	($blockFile,$junk) = split("::",$blockFiles); #sometimes there are 3 spaces, sometimes two... 
+	$blockFile=~s/\s+$//; #right trim to get rid of any spaces at the end 
+	
+	print "Filename: $blockFile\n";
+
+	if ($blockFile eq "") {
+		print "Error with the connection information. Please make sure to use correct connection name for the given part and port\n";
+		exit (-1);
+	}
+	
+	my $fileContents = getFileContents($blockFile);
+
+	my $rmID = findRmid($portName, $fileContents, "IPort"); 
+	
+
+	my @fileContents_arr = split("\n",$fileContents);
+	
+	my $inPort = "false"; 
+	my $inCorrectPort = "false";
+	my $inElement = "false";
+	my $inCorrectElement = "false";
+	
+	open(BUFFERINIT, ">",$blockFile);
+	print BUFFERINIT "";
+	close(BUFFERINIT);
+	
+	open(BUFFER, ">>", $blockFile); 
+	
+	foreach (@fileContents_arr) {
+		chomp($_);
+		my $line = $_; 
+		if (index($line,"<IPort type=\"e\">")!=-1) {$inPort = "true";}
+		if (index($line,"<ELEMENT>")!=-1){$inElement = "true";}
+		if (index($line,"<\/IPort>")!=-1){
+			$inPort = "false";
+			$inCorrectPort = "false";
+		}
+		if (index($line,"<\/ELEMENT")!=-1){
+			$inElement = "false";
+			$inCorrectElement = "false";
+		}
+		
+		if (($inPort eq "false") && ($inElement eq "false")) {
+			print BUFFER $line . "\n"; 			
+		}
+		
+		else {
+# in Part lets check if that is the right one. 
+			if (index($line,"<_id type=\"a\">$portID")!=-1){
+				$inCorrectPort = "true"; 
+			}
+			
+			if (index($line, "<ID>$rmID</ID>")!=-1){
+				$inCorrectElement = "true";
+			}
+
+			if (($inCorrectPort eq "true") || ($inCorrectElement eq "true")) {
+				next;
+			}
+			else {
+				print BUFFER $line . "\n";
+			}
+		}
+	
+	}
+	close (BUFFER); 	
+
+
+	my $fileContents = getFileContents($blockFile);
+	my @fileContents_arr = split("\n",$fileContents);
+	
+	open (BUFFERR, ">", $blockFile);
+	my $prev_line = "";
+	my $prev_line_print = "";
+	my $rm_prev_line = "";
+	my $rm_prev_line_print = "";
+	my $secSkip = "false";
+	foreach(@fileContents_arr) {
+	
+		my $current_line = $_; 	
+		chomp($current_line);
+		$current_line =~s/\s//ig; 
+		$current_line =~s/\n//ig;
+		
+		if (index($current_line,"<IPorttype=\"e\">") !=-1) {
+			$prev_line = $current_line;
+			$prev_line_print = $_;
+			next;
+		}
+		if ($prev_line ne "") {
+			if (index($prev_line . $current_line,"<IPorttype=\"e\"><\/IPort>") !=-1){
+				$prev_line = "";
+				$prev_line_print = ""; 
+				next;
+			}
+			else {
+				print BUFFERR "$prev_line_print\n";
+				$prev_line = "";
+				$prev_line_print = ""; 
+				}
+		}
+		
+		
+		if (index($current_line,"<ELEMENT>") !=-1) {
+			$rm_prev_line = $current_line;
+			$rm_prev_line_print = $_;
+			next;
+		}
+		if ($rm_prev_line ne "") {
+			if ((index($rm_prev_line,"<ELEMENT>")!=-1) && (index($current_line,"<\/ELEMENT>")!=-1)){
+				$rm_prev_line = "";
+				$rm_prev_line_print = ""; 
+				next;
+			}
+			else {
+				print BUFFERR "$rm_prev_line_print\n";
+				$rm_prev_line = "";
+				$rm_prev_line_print = ""; 
+				}
+		}
+		
+	
+
+		print BUFFERR "$_\n";
+
+	}	
+	close (BUFFERR);
+	
+	
+	fixRhapsodyIndicies($blockFile);	
+	
+
+	
 
 }
 
@@ -367,8 +683,7 @@ sub deleteConnector {
 			next;
 		}
 		if ($rm_prev_line ne "") {
-		print "$rm_prev_line" . "$current_line\n";
-			if (index($rm_prev_line . $current_line,"<ELEMENT><\/REFERENCES>") !=-1){
+			if ((index($rm_prev_line,"<ELEMENT>")!=-1) && (index($current_line,"<\/ELEMENT>")!=-1)){
 				$rm_prev_line = "";
 				$rm_prev_line_print = ""; 
 				next;
